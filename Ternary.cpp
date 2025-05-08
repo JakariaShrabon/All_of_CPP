27daf2f2b6e1de5a5744f53f4a7772a5afdfab2e
Jakaria_Shrabon <133669270+JakariaShrabon@users.noreply.github.com>
2025-05-08 16:16:44 +0600

    Add files via upload

---
+++ Ternary.cpp
@@ -0,0 +1,12 @@
+// why we want to use ternary operator in cpp language
+// we are usally use if else operator
+// but if  we use ternary operator we can use if else in short write of code
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int marks = 45;
+    marks >= 40 ? cout << "You are pass" : cout << "You are fail";
+    system("pause>0");
+}
